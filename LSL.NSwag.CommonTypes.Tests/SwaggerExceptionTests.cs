3106e6accfe7c79cb92587bc13b3e8f522d39531
Alun Jones <alunacjones@gmail.com>
2023-07-08 08:42:58 +0100

    get full branch coverage too

--- LSL.NSwag.CommonTypes.Tests/SwaggerExceptionTests.cs
+++ LSL.NSwag.CommonTypes.Tests/SwaggerExceptionTests.cs
@@ -1,6 +1,8 @@
 using System;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
 using FluentAssertions;
 using LSL.NSwag.CommonTypes.Client;
 using NUnit.Framework;
@@ -59,6 +61,54 @@ namespace LSL.NSwag.CommonTypes.Tests
 
             result.Should()
             .Be("HTTP Response: \n\na response\n\nLSL.NSwag.CommonTypes.Client.SwaggerException: my message\n\nStatus: 400\nResponse: \na response\r\n ---> System.Exception: Exception of type 'System.Exception' was thrown.\r\n   --- End of inner exception stack trace ---");
-        }
+        }       
+
+        [Test]
+        public void GivenASwaggerExceptionIsCreated_ItShouldHaveTheExpectedToStringValueWithANullResponse()
+        {
+            var result = new SwaggerException(
+                "my message", 
+                400, 
+                null, 
+                new ReadOnlyDictionary<string, IEnumerable<string>>(new Dictionary<string, IEnumerable<string>>
+                {
+                    ["Header"] = new List<string>
+                    {
+                        "header-value"
+                    }
+                }),
+                new Exception()
+            )
+            .ToString();
+
+            result.Should()
+            .Be("HTTP Response: \n\n\n\nLSL.NSwag.CommonTypes.Client.SwaggerException: my message\n\nStatus: 400\nResponse: \n(null)\r\n ---> System.Exception: Exception of type 'System.Exception' was thrown.\r\n   --- End of inner exception stack trace ---");
+        }                       
+
+        [Test]
+        public void GivenASwaggerExceptionIsCreated_ItShouldHaveTheExpectedToStringValueWithALongResponse()
+        {
+            var response = Enumerable.Range(0, 600).Aggregate(new StringBuilder(), (sb, i) => sb.Append("a")).ToString();
+            var expectedResponse = response.Substring(0, 512);
+            
+            var result = new SwaggerException(
+                "my message", 
+                400, 
+                response, 
+                new ReadOnlyDictionary<string, IEnumerable<string>>(new Dictionary<string, IEnumerable<string>>
+                {
+                    ["Header"] = new List<string>
+                    {
+                        "header-value"
+                    }
+                }),
+                new Exception()
+            )
+            .ToString();
+
+            result
+                .Should()
+                .Be($"HTTP Response: \n\n{response}\n\nLSL.NSwag.CommonTypes.Client.SwaggerException: my message\n\nStatus: 400\nResponse: \n{expectedResponse}\r\n ---> System.Exception: Exception of type 'System.Exception' was thrown.\r\n   --- End of inner exception stack trace ---");
+        }       
     }
 }
