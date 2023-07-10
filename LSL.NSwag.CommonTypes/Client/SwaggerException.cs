b16c9aa6b3857b63d1c2d7fabdfaca33cd518482
Alun Jones <alunacjones@gmail.com>
2023-07-10 09:35:54 +0100

    generic type

--- LSL.NSwag.CommonTypes/Client/SwaggerException.cs
+++ LSL.NSwag.CommonTypes/Client/SwaggerException.cs
@@ -48,4 +48,32 @@ namespace LSL.NSwag.CommonTypes.Client
             return string.Format("HTTP Response: \n\n{0}\n\n{1}", Response, base.ToString());
         }
     }
+
+    /// <summary>
+    /// The common generic exception type for NSwag generated clients
+    /// </summary>
+    /// <typeparam name="TResult"></typeparam>
+    public class SwaggerException<TResult> : SwaggerException
+    {
+        /// <summary>
+        /// The result of the client call
+        /// </summary>
+        /// <value></value>
+        public TResult Result { get; private set; }
+
+        /// <summary>
+        /// NSwag-based constructor for the erroneous call
+        /// </summary>
+        /// <param name="message">The message from the client implementation</param>
+        /// <param name="statusCode">The status code that was returned by the API</param>
+        /// <param name="response">The response from the API</param>
+        /// <param name="headers">The headers that were returned in the erroneous call</param>
+        /// <param name="result"></param>
+        /// <param name="innerException">Any inner exception</param>        
+        public SwaggerException(string message, int statusCode, string response, System.Collections.Generic.IReadOnlyDictionary<string, System.Collections.Generic.IEnumerable<string>> headers, TResult result, System.Exception innerException)
+            : base(message, statusCode, response, headers, innerException)
+        {
+            Result = result;
+        }
+    }    
 }
